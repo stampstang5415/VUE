9ee82d8cce9a9a27eb284f8079c2ab689c14b45b
stampstang5415 <stampstang5415@gmail.com>
2018-12-15 17:02:54 +0700

    all2

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,9 +1,59 @@
-<template>
-    <div>
-        <h1>Product List</h1>   
-         <b-table striped hover :items="products" :fields="fields" :per-page="pageSize" :current-page="pageIndex" ></b-table>
-           <b-pagination size="md" :total-rows="products.length" v-model="pageIndex" :per-page="pageSize"></b-pagination>
-    </div>
+ <template>
+ <b-row>
+   
+      <b-col md="6" class="my-1">
+        <b-form-group horizontal label="Filter" class="mb-0">
+          <b-input-group>
+            <b-form-input v-model="filter" placeholder="Type to Search" />
+            <b-input-group-append>
+              <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+
+
+  <b-table striped hover :items="items" :fields="fields" :filter="filter" :per-page="pageSize" :current-page="pageIndex">
+    <template slot="show_details" slot-scope="row">
+      <!-- we use @click.stop here to prevent emitting of a 'row-clicked' event  -->
+      <b-button size="sm" @click.stop="row.toggleDetails" class="mr-2">
+       {{ row.detailsShowing ? 'Hide' : 'Show'}} Details
+      </b-button>
+      <!-- In some circumstances you may need to use @click.native.stop instead -->
+      <!-- As row.showDetails is one-way, we call the toggleDetails function on @change -->
+     
+    </template>
+    <template slot="row-details" slot-scope="row">
+      <b-card>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right"><b>category_id:</b></b-col>
+          <b-col>{{ row.item.category_id }}</b-col>
+        </b-row>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right"><b>category_name:</b></b-col>
+          <b-col>{{ row.item.category_name }}</b-col>
+        </b-row>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right"><b>description:</b></b-col>
+          <b-col>{{ row.item.description }}</b-col>
+        </b-row>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right"><b>picture:</b></b-col>
+          <b-col>{{ row.item.picture }}</b-col>
+        </b-row>
+        <b-button size="sm" @click="row.toggleDetails">Hide Details</b-button>
+      </b-card>
+    </template>
+  </b-table>
+   <b-col md="6" class="my-1">
+  <b-pagination align="center" size="md" :total-rows="items.length" v-model="pageIndex" :per-page="pageSize">
+    </b-pagination> 
+     </b-col>
+    <br>
+ <b-col md="6" class="my-1">
+    <div>CurrentPage: {{pageIndex}}</div>  
+     </b-col>
+  </b-row>
 </template>
 <script>
 import axios from "axios";
